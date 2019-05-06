baf9e4dbdcca10ee5e98686fcda64b3d6bac77b5
Stuart Dobson <stuartdobson@outlook.com>
2019-05-06 19:28:33 +1000

    Add configuration to loop through a folder of csvs

--- CSVTest/Program.cs
+++ CSVTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.IO;
 
 namespace CSVTest
@@ -9,41 +10,38 @@ namespace CSVTest
 		{
 			try
 			{
-				if (args.Length < 1)
+				var builder = new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("appsettings.json", optional: true);
+				var configuration = builder.Build();
+				string filePath = configuration["FilePath"];
+
+				foreach (var file in Directory.GetFiles(filePath, "*.csv", SearchOption.AllDirectories))
 				{
-					Console.WriteLine("Please enter a filename argument");
-					return;
-				}
-
-				string file = args[0];
-
-				// Testing //
-				// string file = "LP_210095893_20150901T011608049.csv";
-				// string file = "TOU_212621145_20150911T022358.csv";
-
-
-				if (File.Exists(file))
-				{
-					var reader = new CSVReader(file);
-					reader.Read();
-
-					if (file.StartsWith("TOU"))
+					string fileName = Path.GetFileName(file);
+					if (File.Exists(fileName))
 					{
-						var pro = new TOUProcessor(reader);
-						var alert = new ReadingAlerter<TOUProcessor>(pro);
-						alert.PrintAlerts(file);
-					}
-					else if (file.StartsWith("LP"))
-					{
-						var pro = new LPProcessor(reader);
-						var alert = new ReadingAlerter<LPProcessor>(pro);
-						alert.PrintAlerts(file);
-					}
+						var reader = new CSVReader(file);
+						reader.Read();
 
-				}
-				else
-				{
-					Console.WriteLine("File does not exist");
+						if (fileName.StartsWith("TOU"))
+						{
+							var pro = new TOUProcessor(reader);
+							var alert = new ReadingAlerter<TOUProcessor>(pro);
+							alert.PrintAlerts(fileName);
+						}
+						else if (fileName.StartsWith("LP"))
+						{
+							var pro = new LPProcessor(reader);
+							var alert = new ReadingAlerter<LPProcessor>(pro);
+							alert.PrintAlerts(fileName);
+						}
+
+					}
+					else
+					{
+						Console.WriteLine("File does not exist");
+					}
 				}
 
 				Console.WriteLine("Press any key to exit...");
